308d79fe6878dbfc19e9dc7e056e2e79d0bd1f5f
Rod2 <coronadocastar@hotmail.com>
2024-12-09 12:35:40 -0600

    Adding git ignore and cello-service

---
+++ cello-service/src/main/java/com/rodcor/cello_service/CelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rodcor.cello_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelloServiceApplication.class, args);
+	}
+
+}
